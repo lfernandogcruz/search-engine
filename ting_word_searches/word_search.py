9d3b77c30b146f08cce52e5ce47701cc4ba82cc6
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-02-10 18:16:00 -0300

    feat: req 07 - implements the function EXISTS_WORD on the word_search module

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,26 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+    lowered_word = word.lower()
+
+    for index in range(instance.__len__()):
+        file_name = instance.search(index)["nome_do_arquivo"]
+
+        res = {
+            "palavra": lowered_word,
+            "arquivo": file_name,
+            "ocorrencias": [],
+        }
+
+        rows = instance.search(index)["linhas_do_arquivo"]
+
+        for row in rows:
+            if (row.lower()).__contains__(lowered_word):
+                res["ocorrencias"].append({"linha": (rows.index(row) + 1)})
+
+        if res["ocorrencias"]:
+            result.append(res)
+
+    return result
 
 
 def search_by_word(word, instance):
